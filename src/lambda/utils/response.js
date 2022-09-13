c2041ee6df50502d36c92a0f5724a9772462ed19
jacksonbeaman <jacksonbeaman@gmail.com>
2022-09-12 19:41:24 -0700

    Updates CORS for API

--- src/lambda/utils/response.js
+++ src/lambda/utils/response.js
@@ -2,10 +2,11 @@ const generate = (statusCode, body) => ({
   statusCode,
   body,
   headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST, PUT, GET, OPTIONS',
     'Access-Control-Allow-Headers':
-      'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+      'Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
+    'X-Requested-With': '*',
   },
 });
 
