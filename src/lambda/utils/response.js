216188e85689868dd83101c8c6b562cd0b15eec0
jacksonbeaman <jacksonbeaman@gmail.com>
2022-09-12 19:10:37 -0700

    Update to allow all 'Access-Control-Allow-Origin'

--- src/lambda/utils/response.js
+++ src/lambda/utils/response.js
@@ -2,7 +2,7 @@ const generate = (statusCode, body) => ({
   statusCode,
   body,
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, PUT, GET, OPTIONS',
     'Access-Control-Allow-Headers':
       'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
