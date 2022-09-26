8f6a79709e831bf57a2f72276226c2481771ea42
jacksonbeaman <jacksonbeaman@gmail.com>
2022-09-26 13:56:44 -0700

    Implements response generator in postFundAccount lambda

--- src/lambda/postFundAccount/index.js
+++ src/lambda/postFundAccount/index.js
@@ -1,5 +1,6 @@
 const getUser = require('../../dynamodb/getUser/index.js');
 const recordTransaction = require('../../dynamodb/recordTransaction/index.js');
+const response = require('../utils/response');
 
 exports.handler = async (event, context) => {
   try {
@@ -12,12 +13,10 @@ exports.handler = async (event, context) => {
 
     const res = await recordTransaction(username, cashToDeposit, 'deposit');
 
-    return { statusCode: 200, body: JSON.stringify(res) };
+    return response.generate(200, JSON.stringify(res));
   } catch (error) {
-    console.error(error.statusCode, error.message);
-    return {
-      statusCode: error.statusCode ? error.statusCode : 400,
-      body: JSON.stringify(error.message ? error.message : error),
-    };
+    const errorCode = error.statusCode ? error.statusCode : 400;
+    const errorBody = error.message ? error.message : error;
+    return response.generate(errorCode, errorBody);
   }
 };
